2bdc0416e20b58b19bef1ecd951d5144f294524e
jaydenmetcalfe17 <jayden10.9@gmail.com>
2025-05-12 15:24:17 -0700

    Updating deployment

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
+// import {
+//   Route,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   RouterProvider
+// } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
@@ -13,22 +14,35 @@ import ExperiencePage from './pages/ExperiencePage'
 import ProjectsPage from './pages/ProjectsPage'
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="experience" element={<ExperiencePage />} />
-        <Route path="projects" element={<ProjectsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    ),
-    {
-      // 👇 This must match the "homepage" field in package.json
-      basename: "/HTML-Portfolio"
-    }
+  return ( 
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="experience" element={<ExperiencePage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   )
-
-  return <RouterProvider router={router} />
 }
 
 export default App
+
+// const App = () => {
+//   const router = createBrowserRouter(
+//     createRoutesFromElements(
+//       <Route path="/" element={<MainLayout />}>
+//         <Route index element={<HomePage />} />
+//         <Route path="experience" element={<ExperiencePage />} />
+//         <Route path="projects" element={<ProjectsPage />} />
+//         <Route path="*" element={<NotFoundPage />} />
+//       </Route>
+//     )
+//   )
+
+//   return <RouterProvider router={router} />
+// }
+
+
