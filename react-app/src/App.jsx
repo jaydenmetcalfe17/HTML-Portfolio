ce8b8bfd35395d19df5fc299655fd5ff9634b39b
jaydenmetcalfe17 <jayden10.9@gmail.com>
2025-05-12 15:08:10 -0700

    Fixing GH deployment

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import ExperiencePage from './pages/ExperiencePage';
-import ProjectsPage from './pages/ProjectsPage';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import ExperiencePage from './pages/ExperiencePage'
+import ProjectsPage from './pages/ProjectsPage'
 
 const App = () => {
-  
-  
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element = {<MainLayout/>}>
-        <Route index element={<HomePage/>}/>
-        <Route path="/experience" element={<ExperiencePage/>}/>
-        <Route path="/projects" element={<ProjectsPage/>}/>
-        <Route path="*" element={<NotFoundPage/>}/>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
-    )
-  );
+    ),
+    {
+      // 👇 This must match the "homepage" field in package.json
+      basename: "/HTML-Portfolio"
+    }
+  )
 
-  return <RouterProvider router={router}/>;
-};
+  return <RouterProvider router={router} />
+}
 
 export default App
