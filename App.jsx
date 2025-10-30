cf58639d6ca719502ce9b2cdd0fe65108e221765
jaydenmetcalfe17 <jayden10.9@gmail.com>
2025-10-30 09:38:26 -0700

    switching to createBrowserRouter

--- App.jsx
+++ App.jsx
@@ -6,7 +6,8 @@ import {
   RouterProvider, 
   HashRouter,
   Routes,
-  BrowserRouter
+  BrowserRouter,
+  ScrollRestoration
 } from 'react-router-dom'
 
 import MainLayout from '/src/layouts/MainLayout'
@@ -15,37 +16,56 @@ import NotFoundPage from '/src/pages/NotFoundPage'
 import ExperiencePage from '/src/pages/ExperiencePage'
 import ProjectsPage from '/src/pages/ProjectsPage'
 
-const App = () => {
-  return ( 
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="experience" element={<ExperiencePage />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-export default App
-
 // const App = () => {
-//   const router = createBrowserRouter(
-//     createRoutesFromElements(
-//       <Route path="/" element={<MainLayout />}>
-//         <Route index element={<HomePage />} />
-//         <Route path="experience" element={<ExperiencePage />} />
-//         <Route path="projects" element={<ProjectsPage />} />
-//         <Route path="*" element={<NotFoundPage />} />
-//       </Route>
-//     )
+//   return ( 
+//     <BrowserRouter basename={import.meta.env.BASE_URL}>
+//       <Routes>
+//         <Route path="/" element={<MainLayout />}>
+//           <Route index element={<HomePage />} />
+//           <Route path="experience" element={<ExperiencePage />} />
+//           <Route path="projects" element={<ProjectsPage />} />
+//           <Route path="*" element={<NotFoundPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
 //   )
-
-//   return <RouterProvider router={router} />
 // }
+// export default App
 
+const routes = [
+     {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+           {
+             index: true,
+             element: <HomePage />,
+           },
+           {
+             path: "experience",
+             element: <ExperiencePage />,
+           },
+           {
+             path: "projects",
+             element: <ProjectsPage />,
+           },
+           {
+             path: "*",
+             element: <NotFoundPage />,
+           },
+        ],
+      },
+    ];
+
+const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+});
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
 
 
 
